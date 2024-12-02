d6c72018c41898f0aa366c832abda927e059100c
Kenji Saito <ken-yo@mbr.nifty.com>
2024-12-02 22:48:33 +0900

    Bumps node modules

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -6,32 +6,38 @@ import * as process from "process";
 import createClient from "../src/client";
 
 test("throws Parameter not found", async () => {
-  const client = createClient("us-west-2");
-  await expect(client.getParameterValue("not_found")).rejects.toThrow("");
+  if (process.env.AWS_ACCESS_KEY_ID) {
+    const client = createClient("us-west-2");
+    await expect(client.getParameterValue("not_found")).rejects.toThrow("");
+  }
 });
 
 test("Get Parameter", async () => {
-  const client = createClient("us-west-2");
-  const value = await client.getParameterValue("/get-aws-ssm-parameter/test");
-  expect(value).toBe("test");
+  if (process.env.AWS_ACCESS_KEY_ID) {
+    const client = createClient("us-west-2");
+    const value = await client.getParameterValue("/get-aws-ssm-parameter/test");
+    expect(value).toBe("test");
+  }
 });
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test("test runs", () => {
-  process.env["INPUT_PARAMETER-NAME"] = "/get-aws-ssm-parameter/test";
-  process.env["INPUT_AWS-REGION"] = "us-west-2";
-  process.env["INPUT_DECRYPTION"] = "false";
-  const np = process.execPath;
-  const ip = path.join(__dirname, "..", "lib", "main.js");
+  if (process.env.AWS_ACCESS_KEY_ID) {
+    process.env["INPUT_PARAMETER-NAME"] = "/get-aws-ssm-parameter/test";
+    process.env["INPUT_AWS-REGION"] = "us-west-2";
+    process.env["INPUT_DECRYPTION"] = "false";
+    const np = process.execPath;
+    const ip = path.join(__dirname, "..", "lib", "main.js");
 
-  try {
-    const options: cp.ExecFileSyncOptions = {
-      env: process.env,
-    };
-    const ret = cp.execFileSync(np, [ip], options).toString();
-    console.log(ret);
-  } catch (error) {
-    console.error(error);
-    fail(error);
+    try {
+      const options: cp.ExecFileSyncOptions = {
+        env: process.env,
+      };
+      const ret = cp.execFileSync(np, [ip], options).toString();
+      console.log(ret);
+    } catch (error) {
+      console.error(error);
+      fail(error);
+    }
   }
 });
