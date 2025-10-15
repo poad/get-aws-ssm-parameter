288e519388dba2104c50722ea73951fc2e7a0e34
Kenji Saito <ken-yo@mbr.nifty.com>
2025-10-15 21:47:11 +0900

    Bumps node modules

--- update.sh
+++ update.sh
@@ -14,7 +14,7 @@ if [ $result -ne 0 ]; then
 fi
 echo ""
 pwd
-npx -y pnpm@latest self-update && pnpm install -r && pnpm up -r && pnpm lint-fix && pnpm audit --fix && pnpm clean && pnpm build && pnpm package
+npx -y pnpm@latest self-update && pnpm install -r && pnpm up -r && pnpm lint-fix && pnpm audit --fix && pnpm clean && pnpm build && pnpm package && git add dist
 result=$?
 if [ $result -ne 0 ]; then
   cd "${CUR}" || exit
