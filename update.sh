f0ad01a4cd4171c03bdb3af80943ace6dec9f1b0
Kenji Saito <ken-yo@mbr.nifty.com>
2026-04-15 22:47:54 +0900

    Bumps node modules

--- update.sh
+++ update.sh
@@ -5,48 +5,65 @@ CUR=$(pwd)
 CURRENT=$(cd "$(dirname "$0")" || exit;pwd)
 echo "${CURRENT}"
 
-cd "${CURRENT}" || exit
-git pull --prune
-result=$?
-if [ $result -ne 0 ]; then
+result=$(cd "${CURRENT}" || exit)
+if [ "$result" -ne 0 ]; then
   cd "${CUR}" || exit
-  exit $result
-fi
-echo ""
-pwd
-rm -rf dist && npx -y pnpm@latest self-update && pnpm install -r && pnpm up -r && pnpm lint-fix && pnpm audit --fix  && pnpm up -r && pnpm install -r --no-frozen-lockfile && pnpm clean && pnpm build && pnpm package && git add dist
-result=$?
-if [ $result -ne 0 ]; then
-  cd "${CUR}" || exit
-  exit $result
+  exit "$result"
 fi
 
-cd "${CURRENT}/__tests__/cdk" || exit
-git pull --prune
-result=$?
-if [ $result -ne 0 ]; then
+result=$(git pull --prune)
+if [ "$result" -ne 0 ]; then
   cd "${CUR}" || exit
-  exit $result
+  exit "$result"
 fi
 echo ""
 pwd
-pnpm install -r && pnpm up -r
-result=$?
-if [ $result -ne 0 ]; then
+
+# result=$(rm -rf dist && npx -y pnpm@latest self-update && pnpm install -r && pnpm up -r && pnpm lint-fix && pnpm audit --fix  && pnpm up -r && pnpm install -r --no-frozen-lockfile && pnpm clean && pnpm build && pnpm package && git add dist)
+result=$(rm -rf dist && npx -y pnpm@latest self-update && pnpm install -r && pnpm up -r && pnpm lint-fix && pnpm up -r && pnpm install -r --no-frozen-lockfile && pnpm clean && pnpm build && pnpm package && git add dist)
+if [ "$result" -ne 0 ]; then
   cd "${CUR}" || exit
-  exit $result
+  exit "$result"
 fi
-cd "${CURRENT}" || exit
-result=$?
-if [ $result -ne 0 ]; then
+
+result=$(cd "${CURRENT}" || exit)
+if [ "$result" -ne 0 ]; then
   cd "${CUR}" || exit
-  exit $result
+  exit "$result"
 fi
-git commit -am "Bumps node modules" && git push
-result=$?
-if [ $result -ne 0 ]; then
+
+pwd
+
+result=$(cd "${CURRENT}/__tests__/cdk" || exit)
+if [ "$result" -ne 0 ]; then
   cd "${CUR}" || exit
-  exit $result
+  exit "$result"
+fi
+
+result=$(git pull --prune)
+if [ "$result" -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit "$result"
+fi
+echo ""
+pwd
+
+result=$(pnpm install -r && pnpm up -r)
+if [ "$result" -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit "$result"
+fi
+
+result=$(cd "${CURRENT}" || exit)
+if [ "$result" -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit "$result"
+fi
+
+result=$(git commit -am "Bumps node modules" && git push)
+if [ "$result" -ne 0 ]; then
+  cd "${CUR}" || exit
+  exit "$result"
 fi
 
 cd "${CUR}" || exit
